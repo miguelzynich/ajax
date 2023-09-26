38a4b6af6b6f5f810d68bb6730742ee31e72effa
miguelzynich@gmail.com <miguelzynich@gmail.com>
2023-09-26 16:29:35 -0300

    tarefa_feita

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-
 const url = 'https://api.github.com/users';
 const main = document.getElementById('main')
-// let user = 'marcelocant'
 let text = ''
 
 function getUser(user) {
-
     fetch(`${url}/${user}`, {
         method: 'GET',
         headers: {
@@ -14,8 +11,21 @@ function getUser(user) {
     })
         .then((response) => response.json())
         .then((data) => {
-            text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`
-            main.innerHTML = text
+            const userImage = document.createElement('img');
+            userImage.src = data.avatar_url;
+            userImage.alt = 'Foto do usuário';
+
+            const userInfo = document.createElement('p');
+            userInfo.textContent = `${data.name} possui ${data.public_repos} repositórios no GitHub como ${data.login}`;
+
+            const userLocation = document.createElement('p');
+            userLocation.textContent = `Localização: ${data.location || 'Não especificado'}`;
+
+            text += `<p>${data.name} possui ${data.public_repos} repositórios públicos no GitHub como ${data.login}</p>`;
+            main.innerHTML = text;
+            main.appendChild(userImage);
+            main.appendChild(userInfo);
+            main.appendChild(userLocation);
         })
         .catch((error) => console.error('Erro: ', error.message || error))
 
@@ -26,5 +36,3 @@ const userInput = document.getElementById('username')
 userInput.addEventListener('focusout', function(event){
     getUser(event.target.value)
 })
-
-
